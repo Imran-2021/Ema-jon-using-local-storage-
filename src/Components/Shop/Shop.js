9a807a19637912213a8ae088fe741f4c14d513c1
next-hero <mdimranulhaque.8dec20@gmail.com>
2021-09-15 17:03:47 +0600

    card modified

--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -3,15 +3,17 @@ import "./Shop.css"
 import fakeData from '../../fakeData';
 import Products from '../Products/Products';
 import Card from '../Card/Card';
+import { addToDatabaseCart } from '../../utilities/databaseManager';
 const Shop = () => {
     const first10 = fakeData.slice(0, 10)
 
     const [data,setData]=useState(first10)
     const [count, setCount]=useState([])
     const handleEvent =(pd)=>{
-        const card =[...count,pd]
-        setCount(card)
-        console.log(card)
+        const card =[...count,pd];
+        setCount(card);
+        const countt = card.filter(pdd=>pdd.key===pd.key)
+        addToDatabaseCart(pd.key, countt.length);
     }
     
     return (
