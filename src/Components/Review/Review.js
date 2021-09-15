bddc9d1e1b649ca86fcf95fdec73e680b21d6ddf
next-hero <mdimranulhaque.8dec20@gmail.com>
2021-09-15 18:06:17 +0600

    card modified

--- src/Components/Review/Review.js
+++ src/Components/Review/Review.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getDatabaseCart } from '../../utilities/databaseManager';
 
 const Review = () => {
+    const [cart,setCart]=useState([])
+
+    useEffect(() => {
+        const data = getDatabaseCart();
+        console.log(data);
+    })
     return (
         <div>
-            <h4>this is review file.</h4>
+            <h4>this is review file.......</h4>
         </div>
     );
 };
--- src/Components/Rivew/Review.js
+++ src/Components/Rivew/Review.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getDatabaseCart } from '../../utilities/databaseManager';
 
 const Review = () => {
+    const [cart,setCart]= useState([])
+    useEffect(()=>{
+        //cart 
+        const saveCart= getDatabaseCart();
+        console.log(saveCart, "object")
+        console.log("object")
+    })
+    console.log("object")
     return (
         <div>
-            <h4>this is review</h4>
+            <h4>this is reviesw</h4>
         </div>
     );
 };
